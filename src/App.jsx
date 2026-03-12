86527fe0f9d8a04d0b4d149823ad9d2f2bafd973
shadrach_05 <shadrachmulia@gmail.com>
2026-03-12 21:11:22 +0700

    feat: header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import logo from "./assets/react.svg";
+
+export default function App() {
+  return (
+    <div id="centering-div">
+      <header>
+        <p id="heading-eyebrow">From Beginner to Advanced Questions</p>
+        <div id="heading-main">
+          <h1>
+            <img src={logo} alt="react-logo" srcset="" /> The React Flash Cards.
+          </h1>
+          <p>Test your react knowledge by answering the flashcards below</p>
+        </div>
+      </header>
+    </div>
+  );
+}
